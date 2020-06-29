27faadf267fb7f1a8775bdfb1f6b678540f9b192
Jeewoo Chung <awesomejwc@icloud.com>
2020-06-29 15:03:28 +0900

    Add 'C++ Class Templates' from HackerRank/c++ Add 'Multi Level Inheritance' from HackerRank/c++ Add 'Preprocessor Solution' from HackerRank/c++

---
+++ hackerRank/c++/C++ClassTemplates.cpp
@@ -0,0 +1,63 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template<class T>
+class AddElements {
+    private:
+        T elem;
+    public:
+        AddElements (T arg) {
+            elem = arg;
+        }
+        T add(T arg2) {
+            return elem + arg2;
+        }
+};
+
+template<>
+class AddElements<string> {
+    private:
+        string elem;
+    public:
+        AddElements (string arg) {
+            elem = arg;
+        }
+        string concatenate(string arg2) {
+            return elem + arg2;
+        }
+};
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
+
---
+++ hackerRank/c++/PreprocessorSolution.cpp
@@ -0,0 +1,36 @@
+/* Enter your macros here */
+#define toStr(a) #a
+#define INF 100000001
+#define io(s) cin >> s
+#define foreach(list, index) for (int index = 0; index < list.size(); index++)
+#define FUNCTION(name, op) void name(int &x, int y) {if (!(x op y)) x = y;}
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}
