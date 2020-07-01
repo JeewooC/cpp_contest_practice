5d86c873adb5e16b9fae0d9a3f7c65bb1a588429
Jeewoo Chung <awesomejwc@icloud.com>
2020-07-01 14:10:06 +0900

    Modify .gitignore to ignore CMake files and .idea files

--- codeChef/CHFICRM.cpp
+++ codeChef/CHFICRM.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
 bool query(int N, int arr[]) {
     if (arr[0] != 5) {
--- codeChef/CHRL4.cpp
+++ codeChef/CHRL4.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <climits>
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main() {
@@ -11,20 +12,31 @@ int main() {
         cin >> specialNumbers[i];
     }
     
-    long long d[N];
-    fill_n(d, N, LONG_LONG_MAX);    
-    d[0] = specialNumbers[0];
+    long d[N];
+    fill_n(d, N, LONG_MAX);
+    if (specialNumbers[0] == 1) {
+        d[0] = 1;
+    } else {
+        d[0] = log(specialNumbers[0]);
+    }
 
     for (int currentIndex = 0; currentIndex < N-1; currentIndex++) {
         for (int j = currentIndex+1; j-currentIndex <= K; j++) {
             if (j >= N) {
                 break;
             }
-            if (d[currentIndex] * specialNumbers[j] < d[j]) {
-                d[j] = d[currentIndex] * specialNumbers[j];
+            if (d[currentIndex] * specialNumbers[j] == 1) {
+                if (1 < d[j]) {
+                    d[j] = 1;
+                }
+            } else {
+                if (log(d[currentIndex] * specialNumbers[j]) < d[j]) {
+                    d[j] = log(d[currentIndex] * specialNumbers[j]);
+                }
             }
         }
     }
 
-    cout << d[N-1]%1000000007 << endl;
+    long res = exp(d[N-1]);
+    cout << res%1000000007 << endl;
 }
