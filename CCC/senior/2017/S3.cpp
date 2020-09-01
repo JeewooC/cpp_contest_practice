eae07cd0cf2b5e5f14e7eedd202a3ec13380f924
Jeewoo Chung <awesomejwc@icloud.com>
2020-09-01 09:43:24 -0700

    Modify S3 from CCC 2017 15/15

--- CCC/senior/2017/S3.cpp
+++ CCC/senior/2017/S3.cpp
@@ -4,27 +4,31 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(false);
   int N; cin >> N;
-  int sums[4001] = {0};
-  int woods[N];
-
-  for (int i = 0; i < N; i++) {
-    int temp; cin >> temp;
-    woods[i] = temp;
+  long woods[2002] = {0};
+  for (int i = 0;i < N; i++) {
+    long temp; cin >> temp;
+    woods[temp]++;
   }
 
-  int maxSum = 0; // keeping track of the largest sum (no need to check all 4000 later)
-  for (int i = 0; i < N; i++) {
-    for (int j = i+1; j < N; j++) {
-      int sum = woods[i] + woods[j];
-      sums[sum]++;
-      if (sum > maxSum) {
-        maxSum = sum;
+  long sums[4004] = {0};
+  for (int i = 1; i < 2001; i++) {
+    if (woods[i] > 0) {
+      for (int j = i; j < 2001; j++) {
+        if (j == i) {
+          // divide by two because if there is only one piece then it wouldn't add anything
+          // but if there are more than one pieces, it would add the appropriate number of pairs
+          sums[i*2] += woods[i] / 2;
+        } else if (woods[j] > 0) {
+          // add the minimum of these two because the less of the two is how many pairs
+          // of these two lengths can be made
+          sums[i+j] += min(woods[i], woods[j]);
+        }
       }
     }
   }
 
-  int maxLength = 0, combinations = 0;
-  for (int i = 2; i < maxSum+1; i++) {
+  long maxLength = 0, combinations = 0;
+  for (int i = 2; i < 4001; i++) {
     if (sums[i] > maxLength) {
       maxLength = sums[i];
       combinations = 1;
@@ -33,5 +37,5 @@ int main() {
     }
   }
 
-  cout << maxLength << " " << combinations << '\n';
+  cout << maxLength << ' ' << combinations << '\n';
 }
